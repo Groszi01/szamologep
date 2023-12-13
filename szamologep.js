ab6d31a241513cf9e383ec4cb1da92456bc97eec
Groszi01 <oruak@protonmail.com>
2023-12-13 10:21:09 +0100

    Tesztek az IF-el

--- szamologep.js
+++ szamologep.js
@@ -1,2 +1,7 @@
 console.log("Üdvözöllek a számológépben")
 console.log("Válaszd ki a kivánt műveletet \n 1. Összeadás \n 2. Kivonás \n 3. Szorzás \n 4. Osztás")
+muvelet = prompt("Add meg a műveletet: ")
+
+if(prompt == 1) {
+    print("Hello")
+}
