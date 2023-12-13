77297718c1694456b5adc2da21fb705f3e2e28aa
Groszi01 <oruak@protonmail.com>
2023-12-13 10:24:13 +0100

    Új bekérések és IF-ek

--- szamologep.js
+++ szamologep.js
@@ -1,7 +1,17 @@
 console.log("Üdvözöllek a számológépben")
 console.log("Válaszd ki a kivánt műveletet \n 1. Összeadás \n 2. Kivonás \n 3. Szorzás \n 4. Osztás")
 muvelet = prompt("Add meg a műveletet: ")
-
+num1 = prompt("Add meg az első számot: ")
+num2 = prompt("Add meg a második számot: ")
 if(muvelet == 1) {
+    console.log("Hello")
+}
+if(muvelet == 2) {
+    console.log("Hello")
+}
+if(muvelet == 3) {
+    console.log("Hello")
+}
+if(muvelet == 4) {
     console.log("Hello")
 }
